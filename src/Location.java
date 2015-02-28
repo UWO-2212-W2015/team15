4f1e612ec02b898957e27be0d8c52d51ce1d5bbe
AmauriAires <amauriairesfilho@gmail.com>
2015-02-28 17:43:10 -0500

    Update the class Location.java

--- src/Location.java
+++ src/Location.java
@@ -6,9 +6,13 @@ class Location {
     String httpLocation;
     int latitude, longitude;
     
+    Forecast forecast;
+
     Location (String searchString){
-	this.userSearch = searchString;
-	this.httpLocation = userSearch.replace(" ", ",");
+    	this.userSearch = searchString;
+    	this.httpLocation = userSearch.replace(" ", ",");
+    	System.out.println(getHttpLocation());
+    	forecast = new Forecast(getHttpLocation());
     }
 
     Location (String city, String country){
@@ -18,6 +22,7 @@ class Location {
 	builder.append(",");
 	builder.append(country);
 	this.httpLocation = builder.toString();
+	forecast = new Forecast(getHttpLocation());
     }
 
     Location (String city, String province, String country){
@@ -30,6 +35,7 @@ class Location {
 	builder.append(",");
 	builder.append(country);
 	this.httpLocation = builder.toString();
+	forecast = new Forecast(getHttpLocation());
     } 
 
     String getHttpLocation (){
@@ -37,4 +43,3 @@ class Location {
     }
 }
 
-    
