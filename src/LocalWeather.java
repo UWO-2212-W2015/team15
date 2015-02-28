4d26a7c068e07c6f9e6d6cc768c608aab1e1d24c
James Allen <jallen97@uwo.ca>
2015-02-28 04:54:48 -0500

    UI Update

--- src/LocalWeather.java
+++ src/LocalWeather.java
@@ -7,6 +7,13 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
 
 import java.awt.Color;
 import java.awt.Font;
@@ -14,6 +21,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemListener;
 import java.awt.event.WindowAdapter;
+import java.awt.TextArea;
 
 public class LocalWeather extends JFrame implements ActionListener{
 	
@@ -22,9 +30,16 @@ public class LocalWeather extends JFrame implements ActionListener{
 	private JPanel shortTerm;
 	private JPanel longTerm;
 	private SpringLayout layout_1;
-	private JLabel tempNumber = new JLabel ("19");
+	private int temperature = 19;
+	private String temperatureString = "" + temperature;
+	private JLabel tempNumber = new JLabel (temperatureString);
 	private JLabel tempScaleLabel = new JLabel ("°c");
+	private JTable table;
+	private JLabel locationLabel_1;
+	private boolean tempBool = true;
+	private JRadioButton tempRadioCelcius;
 	
+	//Constructor
 	public LocalWeather() {
 		setTitle("Team 15 Weather");
 		setSize(800, 600); // default size is 0,0
@@ -32,25 +47,32 @@ public class LocalWeather extends JFrame implements ActionListener{
 		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
 		
 		
-		//create the tab pages
+		//Create the tab pages
 		createCurrent();
 		createShortTerm();
+		createLongTerm();
 		
-		//create new tabbed pane
+		//Create new tabbed pane
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBackground(Color.LIGHT_GRAY);
 		tabbedPane.setPreferredSize(new Dimension(5, 800));
 		tabbedPane.setMinimumSize(new Dimension(5, 500));
 		tabbedPane.setSize(new Dimension(0, 500));
 		
-		//add the tabs
+		//Add the tabs
 		tabbedPane.addTab("Current", current);
 		tabbedPane.addTab("Short Term Forecast", shortTerm);
 		tabbedPane.addTab("Longterm Forecast", longTerm);
 		getContentPane().add(tabbedPane);
 		
+		JSeparator separator = new JSeparator();
+		layout_1.putConstraint(SpringLayout.NORTH, separator, -2, SpringLayout.SOUTH, tempRadioCelcius);
+		layout_1.putConstraint(SpringLayout.WEST, separator, 6, SpringLayout.EAST, tempScaleLabel);
+		layout_1.putConstraint(SpringLayout.SOUTH, separator, 0, SpringLayout.SOUTH, tempRadioCelcius);
+		layout_1.putConstraint(SpringLayout.EAST, separator, 6, SpringLayout.EAST, tempScaleLabel);
+		current.add(separator);
 		
-		//add menu bar and menus
+		//Add menu bar and menus
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
@@ -61,40 +83,68 @@ public class LocalWeather extends JFrame implements ActionListener{
 		
 	  }
 	
+	//Current weather panel
 	public void createCurrent(){
+		
 		//Set layout
 		layout_1 = new SpringLayout();
 		current = new JPanel();
 		current.setLayout(layout_1);
 		
-		//Location Label
-		JLabel locationLabel = new JLabel ("Location");
-		locationLabel.setBounds(100,100,150,20);
-		locationLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		current.add(locationLabel);
-		layout_1.putConstraint(SpringLayout.WEST, locationLabel, 20, SpringLayout.WEST, current);
 		
-		//Temperature Label
+		//Textpane
+		DefaultStyledDocument document = new DefaultStyledDocument();
+		JTextPane textPane = new JTextPane(document);
+		//new style
+		StyleContext context = new StyleContext();
+		
+		//Attribute set
+		Font f = new Font("Tahoma" , Font.PLAIN, 18);
+		textPane.setFont(f);
+		AttributeSet aset = context.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, Color.red);
+		
+		Style style = context.addStyle("test", null);
+		textPane.setEditable(false);
+		String data = "Wind" + "\n" + "\n" + 
+				"Pressure" + "\n" +  "\n" + 
+				"Humidity" + "\n" +  "\n" ;
+		try {
+			document.insertString(0, data, style);
+		} catch (BadLocationException e) {
+			e.printStackTrace();
+		}
+		textPane.setCharacterAttributes(aset, false);
+		layout_1.putConstraint(SpringLayout.NORTH, textPane, 39, SpringLayout.NORTH, current);
+		layout_1.putConstraint(SpringLayout.WEST, textPane, 100, SpringLayout.EAST, tempNumber);
+		current.add(textPane);
+		
+		//Location label
+		locationLabel_1 = new JLabel ("Location");
+		locationLabel_1.setBounds(100,100,150,20);
+		locationLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		current.add(locationLabel_1);
+		
+		//Temperature label
 		tempNumber.setFont(new Font("Tahoma", Font.PLAIN, 90));
+		layout_1.putConstraint(SpringLayout.WEST, locationLabel_1, 20, SpringLayout.WEST, current);
 		current.add(tempNumber);
 		layout_1.putConstraint(SpringLayout.WEST, tempNumber, 120, SpringLayout.WEST, current);
-		layout_1.putConstraint(SpringLayout.NORTH, tempNumber, 40, SpringLayout.NORTH, locationLabel);
+		layout_1.putConstraint(SpringLayout.NORTH, tempNumber, 20, SpringLayout.NORTH, locationLabel_1);
 		
-		//Temperature Scale Label
 		this.tempScaleLabel.setFont(new Font("Tahoma", Font.PLAIN, 60));
 		current.add(this.tempScaleLabel);
 		layout_1.putConstraint(SpringLayout.WEST, this.tempScaleLabel, 5, SpringLayout.EAST, tempNumber);
-		layout_1.putConstraint(SpringLayout.NORTH, this.tempScaleLabel, 40, SpringLayout.NORTH, locationLabel);
+		layout_1.putConstraint(SpringLayout.NORTH, this.tempScaleLabel, 25, SpringLayout.NORTH, locationLabel_1);
 		
-		//Temperature Radio Celcius
-		JRadioButton tempRadioCelcius = new JRadioButton("Celcius", true);
+		//Temperature radio celcius
+		tempRadioCelcius = new JRadioButton("Celcius", true);
 		current.add(tempRadioCelcius);
 		layout_1.putConstraint(SpringLayout.NORTH, tempRadioCelcius, 6, SpringLayout.SOUTH, tempNumber);
 		layout_1.putConstraint(SpringLayout.WEST, tempRadioCelcius, 120, SpringLayout.WEST, current);
 		tempRadioCelcius.addActionListener(this);
 		tempRadioCelcius.setActionCommand("c");
 		
-		//Temperature Radio Fahrenheit
+		//Temperature radio fahrenheit
 		JRadioButton tempRadioFahrenheit = new JRadioButton("Fahrenheit", false);
 		current.add(tempRadioFahrenheit);
 		layout_1.putConstraint(SpringLayout.NORTH, tempRadioFahrenheit, 6, SpringLayout.SOUTH, tempNumber);
@@ -103,11 +153,12 @@ public class LocalWeather extends JFrame implements ActionListener{
 		tempRadioFahrenheit.setActionCommand("f");
 		
 		ButtonGroup group = new ButtonGroup();
-        group.add(tempRadioCelcius);
-        group.add(tempRadioFahrenheit);
+		group.add(tempRadioCelcius);
+		group.add(tempRadioFahrenheit);
 		
 	}
 	
+	//Short term forecast panel
 	public void createShortTerm(){
 		//Set layout
 		SpringLayout layout = new SpringLayout();
@@ -122,13 +173,14 @@ public class LocalWeather extends JFrame implements ActionListener{
 		layout.putConstraint(SpringLayout.WEST, locationLabel, 20, SpringLayout.WEST, current);
 	}
 	
+	//Long term forecast panel
 	public void createLongTerm(){
 		//Set layout
 		SpringLayout layout = new SpringLayout();
 		longTerm = new JPanel();
 		longTerm.setLayout(layout);
 		
-		//Location Label
+		//Location label
 		JLabel locationLabel = new JLabel ("Location");
 		locationLabel.setBounds(100,100,150,20);
 		locationLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
@@ -136,13 +188,21 @@ public class LocalWeather extends JFrame implements ActionListener{
 		layout.putConstraint(SpringLayout.WEST, locationLabel, 20, SpringLayout.WEST, current);
 	}
 	
-	//Radio Celcius / Fahrenheit
+	//Radio celcius / fahrenheit conversions
 	public void actionPerformed(ActionEvent ae) {
 		String action = ae.getActionCommand();
-		if (action.equals("c")) {
+		if (action.equals("c") && tempBool == false) {
 			this.tempScaleLabel.setText("°c");
-		} else if (action.equals("f")){
+			this.temperature = (this.temperature-30)/2;
+			this.temperatureString = ""+temperature;
+			this.tempNumber.setText(temperatureString);
+			tempBool = true;
+		} else if (action.equals("f") && tempBool == true){
 			this.tempScaleLabel.setText("°f");
+			this.temperature = (this.temperature*2)+30;
+			this.temperatureString = ""+temperature;
+			this.tempNumber.setText(temperatureString);
+			tempBool = false;
 		}
 	}
 
