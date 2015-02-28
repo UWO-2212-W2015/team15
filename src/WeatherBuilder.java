9de6fe860d6ffc07f88ee8cfd6f5a1cadffc1a12
AmauriAires <amauriairesfilho@gmail.com>
2015-02-28 17:49:41 -0500

    Update the class WeatherBuilder.java

--- src/WeatherBuilder.java
+++ src/WeatherBuilder.java
@@ -6,7 +6,7 @@ public class WeatherBuilder extends JSONObject{
 	
 	JSONFetcher json = new JSONFetcher();
 	
-	Weather weather = new Weather();
+	Weather weather;
 	private Object weatherInformation = new Object();
 	private String[] parts = new String[10];
 	
@@ -19,6 +19,7 @@ public class WeatherBuilder extends JSONObject{
 	
 	/* Current Weather */
 	public Weather buildCurrent(){
+		weather = new Weather();
 		JSONObject currentWeather = json.getCurrentWeather(location);
 		
 		/* main */
@@ -114,6 +115,7 @@ public class WeatherBuilder extends JSONObject{
 	
 	/* Short Term */
 	public Weather buildShortTerm(){
+		weather = new Weather();
 		JSONObject shortTerm = json.get3HourForecast(location);
 		int cont, lastIndex;
 		
@@ -178,6 +180,7 @@ public class WeatherBuilder extends JSONObject{
 	
 	/* Long term */
 	public Weather buildLongTerm(){
+		weather = new Weather();
 		JSONObject longTerm = json.getDailyForecast(location);
 		int cont, lastIndex;
 		
@@ -280,6 +283,7 @@ public class WeatherBuilder extends JSONObject{
 	
 	/* Mars Current Weather*/
 	public Weather buildMarsWeather(){
+		weather = new Weather();
 		JSONObject marsWeather = json.getMarsJason();
 		int lastIndex;
 		
@@ -397,3 +401,4 @@ public class WeatherBuilder extends JSONObject{
 		return weather;
 	}
 }
+
