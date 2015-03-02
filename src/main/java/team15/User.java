3340f02be6c933d2664e7fa7d99d61006c5788cd
mnardilli <mnardill@uwo.ca>
2015-03-01 20:31:34 -0500

    Update User.java

--- src/main/java/team15/User.java
+++ src/main/java/team15/User.java
@@ -1,10 +1,5 @@
-<<<<<<< HEAD:src/User.java
 import java.util.ArrayList;
-=======
 package team15;
-
-import java.util.List;
->>>>>>> 5d93f0f532c5d11f32c0c048362055eb01906b22:src/main/java/team15/User.java
 
 class User {
 
@@ -26,17 +21,13 @@ class User {
 	return name;
     }
     
-<<<<<<< HEAD:src/User.java
+
     ArrayList<Location> getLocationList(){
 	return locList;
     }
     
     void setCurrentLoc (Location loc){
 	currentLocation = loc;
-=======
-    Location getLocations(){
-        return null; //Stub
->>>>>>> 5d93f0f532c5d11f32c0c048362055eb01906b22:src/main/java/team15/User.java
     }
 
     Location getCurrentLocation(){
