113f1bf2570c5d369091e697a3602c02ffb23b21
Michael Nardilli <mnardill@uwo.ca>
2015-03-29 04:41:09 -0400

    Refine Preferences

--- src/main/java/team15/GUI/PreferencesDialog.java
+++ src/main/java/team15/GUI/PreferencesDialog.java
@@ -10,6 +10,9 @@ package team15.GUI;
 //Imports
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Dimension;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JDialog;
@@ -23,7 +26,7 @@ import java.awt.Color;
 public class PreferencesDialog extends JDialog{
     //Check boxes
     private static JCheckBox tempChk, windChk, pressureChk, 
-                     humidityChk, minMaxChk, sunChk, skyCondChk, celChk, fahChk;
+	humidityChk, minMaxChk, sunChk, skyCondChk, celChk, fahChk;
     
     //A field to track if the dialog successfully updates the settings
     private static boolean updated;
@@ -45,16 +48,18 @@ public class PreferencesDialog extends JDialog{
         Preferences pref = user.pref;
         
         //Layout and panel settings
+	JPanel root = new JPanel();
+	root.setLayout(new BoxLayout(root, BoxLayout.Y_AXIS));
         SpringLayout layout = new SpringLayout();
         JPanel panel = new JPanel();
         panel.setLayout(layout);
-        
+	panel.setBorder(BorderFactory.createTitledBorder("Current Weather Show/Hide Options"));
+	panel.setBackground(BGCOLOR);
+
         //Set up the dialog parameters
         this.setResizable(false);
-        this.getContentPane().add(panel);
-        this.setSize(350, 300);
+        this.setSize(400, 350);
         this.setLocation(200, 200);
-        panel.setBackground(BGCOLOR);
 
         //Add all the checkboxes to the dialog
         //Tempriture check box
@@ -62,11 +67,11 @@ public class PreferencesDialog extends JDialog{
         tempChk.setForeground(txtC);
         tempChk.setBackground(BGCOLOR);
         tempChk.setToolTipText("Check or uncheck this box to "
-                + "show/hide the temperature");
+			       + "show/hide the temperature");
         layout.putConstraint(SpringLayout.WEST, tempChk, 10, 
-                                                SpringLayout.WEST, panel);
+			     SpringLayout.WEST, panel);
         layout.putConstraint(SpringLayout.NORTH, tempChk, 
-                                           10, SpringLayout.NORTH, panel);
+			     10, SpringLayout.NORTH, panel);
         panel.add(tempChk);
 
         //Wind check box
@@ -74,11 +79,11 @@ public class PreferencesDialog extends JDialog{
         windChk.setForeground(txtC);
         windChk.setBackground(BGCOLOR);
         windChk.setToolTipText("Check or uncheck this box to show/hide "
-                + "the wind speed and direction");
+			       + "the wind speed and direction");
         layout.putConstraint(SpringLayout.WEST, windChk, 10, 
-                                                SpringLayout.WEST, panel);
+			     SpringLayout.WEST, panel);
         layout.putConstraint(SpringLayout.NORTH, windChk, 40, 
-                                                   SpringLayout.NORTH, tempChk);
+			     SpringLayout.NORTH, tempChk);
         panel.add(windChk);
         
         //Pressure check box
@@ -86,11 +91,11 @@ public class PreferencesDialog extends JDialog{
         pressureChk.setForeground(txtC);
         pressureChk.setBackground(BGCOLOR);
         pressureChk.setToolTipText("Check or uncheck this box to "
-                + "show/hide the pressure");
+				   + "show/hide the pressure");
         layout.putConstraint(SpringLayout.WEST, pressureChk, 10, 
-                                                SpringLayout.WEST, panel);
+			     SpringLayout.WEST, panel);
         layout.putConstraint(SpringLayout.NORTH, pressureChk, 
-                                               40, SpringLayout.NORTH, windChk);
+			     40, SpringLayout.NORTH, windChk);
         panel.add(pressureChk);
 
         //Sky condition check box
@@ -98,47 +103,36 @@ public class PreferencesDialog extends JDialog{
         skyCondChk.setForeground(txtC);
         skyCondChk.setBackground(BGCOLOR);
         skyCondChk.setToolTipText("Check or uncheck this box to "
-                + "show/hide the sky condition");
+				  + "show/hide the sky condition");
         layout.putConstraint(SpringLayout.WEST, skyCondChk, 10, 
-                                                SpringLayout.WEST, panel);
+			     SpringLayout.WEST, panel);
         layout.putConstraint(SpringLayout.NORTH, skyCondChk, 40, 
-                                                    SpringLayout.NORTH, pressureChk);
+			     SpringLayout.NORTH, pressureChk);
         panel.add(skyCondChk);
         
-        //Celsius check box
-        celChk = new JCheckBox("Celsius", pref.tempUnits);
-        celChk.setForeground(txtC);
-        celChk.setBackground(BGCOLOR);
-        celChk.setToolTipText("Check this box to convert the "
-                + "temperature numbers to celsius");
-        layout.putConstraint(SpringLayout.WEST, celChk, 10, 
-                                                SpringLayout.WEST, panel);
-        layout.putConstraint(SpringLayout.NORTH, celChk, 40, 
-                                               SpringLayout.NORTH, skyCondChk);
-        panel.add(celChk);
 
         //Humidty check box
         humidityChk = new JCheckBox("Humidity", pref.humidity);
         humidityChk.setForeground(txtC);
         humidityChk.setBackground(BGCOLOR);
         humidityChk.setToolTipText("Check or uncheck this box to "
-                + "show/hide the humidity");
-        layout.putConstraint(SpringLayout.WEST, humidityChk, 200, 
-                                                SpringLayout.WEST, panel);
+				   + "show/hide the humidity");
+        layout.putConstraint(SpringLayout.WEST, humidityChk, 250, 
+			     SpringLayout.WEST, panel);
         layout.putConstraint(SpringLayout.NORTH, humidityChk, 10, 
-                                               SpringLayout.NORTH, panel);
+			     SpringLayout.NORTH, panel);
         panel.add(humidityChk);
 
         //Min/max temperature check box
-        minMaxChk = new JCheckBox("Minimum/Maximum", pref.minMaxTemp);
+        minMaxChk = new JCheckBox("Min/Max", pref.minMaxTemp);
         minMaxChk.setForeground(txtC);
         minMaxChk.setBackground(BGCOLOR);
         minMaxChk.setToolTipText("Check or uncheck this box to show/hide "
-                + "the minimum/maximum temperatures of the day");
-        layout.putConstraint(SpringLayout.WEST, minMaxChk, 200, 
-                                                SpringLayout.WEST, panel);
+				 + "the minimum/maximum temperatures of the day");
+        layout.putConstraint(SpringLayout.WEST, minMaxChk, 250, 
+			     SpringLayout.WEST, panel);
         layout.putConstraint(SpringLayout.NORTH, minMaxChk, 40, 
-                                               SpringLayout.NORTH, humidityChk);
+			     SpringLayout.NORTH, humidityChk);
         panel.add(minMaxChk);
 
         //Sunrise/sunset check box
@@ -146,70 +140,98 @@ public class PreferencesDialog extends JDialog{
         sunChk.setForeground(txtC);
         sunChk.setBackground(BGCOLOR);
         sunChk.setToolTipText("Check or uncheck this box to show/hide "
-                + "the sunset/sunrise times of the day");
-        layout.putConstraint(SpringLayout.WEST, sunChk, 200, 
-                                                SpringLayout.WEST, panel);
+			      + "the sunset/sunrise times of the day");
+        layout.putConstraint(SpringLayout.WEST, sunChk, 250, 
+			     SpringLayout.WEST, panel);
         layout.putConstraint(SpringLayout.NORTH, sunChk, 40, 
-                                                 SpringLayout.NORTH, minMaxChk);
-        panel.add(sunChk);
-        
+			     SpringLayout.NORTH, minMaxChk);
+	layout.putConstraint(SpringLayout.SOUTH, panel, 10, 
+			     SpringLayout.SOUTH, sunChk);
+	layout.putConstraint(SpringLayout.EAST, panel, 10, 
+			     SpringLayout.EAST, sunChk);
+        panel.add(sunChk); 
+	root.add(panel);
+	
+	//Create new panel for temperature options
+	panel = new JPanel();
+	panel.setLayout(layout);
+	panel.setBorder(BorderFactory.createTitledBorder("Select Temperature Units"));
+	panel.setBackground(BGCOLOR);
+	root.add(panel);
+        //Celsius check box
+        celChk = new JCheckBox("Celsius", pref.tempUnits);
+        celChk.setForeground(txtC);
+        celChk.setBackground(BGCOLOR);
+        celChk.setToolTipText("Check this box to convert the "
+			      + "temperature numbers to celsius");
+        layout.putConstraint(SpringLayout.WEST, celChk, 10, 
+			     SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, celChk, 10, 
+			     SpringLayout.NORTH, panel);
+        panel.add(celChk);
+
         //Fahrenheit check box
         fahChk = new JCheckBox("Fahrenheit", !pref.tempUnits);
         fahChk.setForeground(txtC);
         fahChk.setBackground(BGCOLOR);
         fahChk.setToolTipText("Check this box to convert the "
-                + "temperature numbers to fahrenheit");
-        layout.putConstraint(SpringLayout.WEST, fahChk, 200, 
-                                                SpringLayout.WEST, panel);
-        layout.putConstraint(SpringLayout.NORTH, fahChk, 0, 
-                                                SpringLayout.NORTH, celChk);
+			      + "temperature numbers to fahrenheit");
+        layout.putConstraint(SpringLayout.WEST, fahChk, 250, 
+			     SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.NORTH, fahChk, 10, 
+			     SpringLayout.NORTH, panel);
+        layout.putConstraint(SpringLayout.EAST, panel, 10, 
+			     SpringLayout.EAST, sunChk);
         panel.add(fahChk);
         
-        /*Add action listeners to make sure only one of Celsius or Fahrenheit
-        is selected at any one time */
-        celChk.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent event){
-                fahChk.setSelected(!celChk.isSelected());
-            }    
-        });
-        fahChk.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent event){
-                celChk.setSelected(!fahChk.isSelected());
-            }    
-        });
-        
+
         //Confirm button
         JButton confirm = new JButton("Confirm");
         confirm.setToolTipText("Click here to save your preferences");
-        layout.putConstraint(SpringLayout.WEST, confirm, 115, 
-                                                SpringLayout.WEST, panel);
+        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, confirm, 0, 
+			     SpringLayout.HORIZONTAL_CENTER, panel);
         layout.putConstraint(SpringLayout.NORTH, confirm, 40, 
-                                                SpringLayout.NORTH, fahChk);
+			     SpringLayout.NORTH, fahChk);
+	layout.putConstraint(SpringLayout.SOUTH, panel, 10, 
+			     SpringLayout.SOUTH, confirm);
         panel.add(confirm); 
-	layout.putConstraint(SpringLayout.EAST, panel, 10, SpringLayout.EAST, minMaxChk);
-	layout.putConstraint(SpringLayout.SOUTH, panel, 10, SpringLayout.SOUTH, confirm);
+
+        
+        /*Add action listeners to make sure only one of Celsius or Fahrenheit
+	  is selected at any one time */
+        celChk.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent event){
+		    fahChk.setSelected(!celChk.isSelected());
+		}    
+	    });
+        fahChk.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent event){
+		    celChk.setSelected(!fahChk.isSelected());
+		}    
+	    });
         
         //Add action to the button
         confirm.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent event){
-                //Save the old preferences
-                Preferences oldPref = user.pref;
-                user.pref = getPref();
+		public void actionPerformed(ActionEvent event){
+		    //Save the old preferences
+		    Preferences oldPref = user.pref;
+		    user.pref = getPref();
                 
-                //Try to save the new user preferences
-                try{
-                    user.saveUser();
-                    updated = true;
-                }
-                //If we failed to save the new preferences default to previous
-                catch(Exception e){
-                    user.pref = oldPref;
-                }
+		    //Try to save the new user preferences
+		    try{
+			user.saveUser();
+			updated = true;
+		    }
+		    //If we failed to save the new preferences default to previous
+		    catch(Exception e){
+			user.pref = oldPref;
+		    }
                 
-                dispose();
-            }    
-        });
+		    dispose();
+		}    
+	    });
         
+	this.add(root);
 	this.pack();
         this.setVisible(true);
     }
